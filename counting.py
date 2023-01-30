cabed00ddf197240cd98044160525b3628632a25
drdayox <53656967+eiskaffe@users.noreply.github.com>
2023-01-30 16:23:00 +0100

    Create counting.py
    Added main file

---
+++ counting.py
@@ -0,0 +1,102 @@
+import random
+RANDOM_OR_MANUAL = 0 
+# 0 for random, 1 for manual
+
+def convert(A: str, length: int = 0) -> str:
+    t = str(bin(int(A)))[2:]
+    return "0" * (length - len(t)) + t
+
+def calculate(A: str, B: str) -> str:
+    # Analytical representation of the XOR gate:
+    # f(a, b) = |a - b|
+    returning = ""
+    for a, b in zip(A, B, strict=True):
+        if abs(int(a) - int(b)): returning += "1"
+        else: returning += "0"
+    return returning 
+
+def bin_to_str(A: str) -> str:
+    L = len(A)
+    c = 0
+    for i, a in enumerate(A):
+        if int(a): c += 2 ** (L - i - 1)
+    return c
+
+def is_prime(n: int) -> bool:
+    if n <= 3: return n > 1
+    if n % 2 == 0 or n % 3 == 0: return False
+    limit = int(n**0.5)
+    for i in range(5, limit+1, 6):
+        if n % i == 0 or n % (i+2) == 0:
+            return False
+    return True
+
+def trial_division(n: int) -> list[int]:
+    factors = []
+    f = 2
+    if n == 1: return [1]         
+    while n > 1:       
+        if n % f == 0: 
+            factors.append(f)
+            n //= f    
+        else: f += 1
+    # Only odd number is possible
+    return factors
+
+def splitter(n: int):
+    if n <= 3: return n
+    for i in range(n):
+        if not is_prime(n - i): break
+    factors = list(map(splitter, trial_division(n - i)))
+    return factors, i
+
+def printing(a: list) -> str:
+    string = [printing_recurse(x) for x in a]
+    return "^".join(string)
+
+def printing_recurse(a) -> str:
+    # [[2, ([2, 2], 1)], [2, 3]]  ---->  2*((2*2)+1)*(2*3)
+    if isinstance(a, list) and all([isinstance(x, int) for x in a]):
+        return f"({'*'.join(map(str, a))})"
+    elif isinstance(a, tuple):
+        list_part, integer_part = a
+        return f"({printing_recurse(list_part)}+{integer_part})"
+    elif isinstance(a, list):
+        lst = [printing_recurse(x) for x in a]
+        return "*".join(lst)
+    elif isinstance(a, int):
+        return str(a)
+
+def random_integers(N: int, b: int) -> list[int]:
+    L = []
+    if N > b: raise ValueError("N can not be larger than b")
+    while len(L) < N:
+        if (a := random.randint(1, b)) not in L: L.append(a)
+    return L
+
+def main() -> None:
+    N = input("Input the the next number! > ")
+    TARGET = convert(N)
+    l = len(TARGET)
+    I = []
+    max_value = 2 ** l - 1
+    if RANDOM_OR_MANUAL: I = input(f"Give the desired numbers with spaces between! (n <= {max_value})! > ").split()
+    else: I = random_integers(int(input(f"Set the number of desired random numbers! (n <= {max_value - 1}) > ")), max_value)
+
+    current = "0"*l
+    for mod in map(lambda x: convert(x, l), I): 
+        current = calculate(current, mod)
+        
+    difference = calculate(TARGET, current)
+    factors = [trial_division(int(n)) for n in I + [bin_to_str(difference)]]
+    factors.sort(key=lambda x: int(x))
+    factors = [[splitter(f) for f in factor] for factor in factors if factor]
+
+    printed = printing(factors)
+    print(printed)
+    # print(printed.replace("*", "\\*"))
+            
+    input("\nPress enter to exit...")
+
+if __name__ == "__main__":
+    main()
